669a517f5ed4934826e003273271a855b4eacdb1
Thiago Silva Andrade <92411755+Fulozz@users.noreply.github.com>
2025-01-02 12:34:29 -0300

    Update login redirection and enhance home page with user information display

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,23 @@
-import Image from "next/image";
+"use client"
+
+import useUser from '@/hooks/useUser'
+import React from 'react'
 
 export default function Home() {
+
+  const user = useUser();
+  console.log(user,"dashboard")
+  if (!user) {
+      return null;
+  }
+
   return (
     <div>
-      <a href="/login">Login</a>
+      <div className="">
+      <h1>Olá, {user.name}!</h1>
+      <p>Seu e-mail é: {user.email}</p>
+      {/* Exibir outras informações do usuário */}
+    </div>
     </div>
   );
 }

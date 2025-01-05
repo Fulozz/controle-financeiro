84429405282d03f4d9d57908d38e3c236da621f5
Thiago Silva Andrade <92411755+Fulozz@users.noreply.github.com>
2025-01-05 17:08:37 -0300

    Add BottomMenu functionality: integrate ModalCadastroPago and ModalCadastroRecebido for improved transaction handling

--- src/components/cadastro/BottomMenu.jsx
+++ src/components/cadastro/BottomMenu.jsx
@@ -1,7 +1,8 @@
 "use client"
 import { Plus } from 'lucide-react'
 import React, { useState } from 'react'
-
+import ModalCadastroPago from '../cadastro/ModalCadastro.Pago'
+import ModalCadastroRecebido from '../cadastro/ModalCadastro.Recebido'
 const BottomMenu = ({ user }) => {
   const [isOpenRecebido, setIsOpenRecebido] = useState(false)
   const [isOpenPago, setIsOpenPago] = useState(false)
@@ -11,9 +12,22 @@ const BottomMenu = ({ user }) => {
         user && user.name && (
           <>
           <button className='cursor-pointer p-4 text-base'><Plus className='h-[30px] w-[40px]'/></button>
-          <div className=""></div>
+          <div className="bg-white shadow-lg bottom-[55px] right-[15px] hidden group-hover:block absolute rounded-lg min-w-[200px] z-10 border border-gray-400">
+            <div onClick={()=> setIsOpenRecebido(true)} className='p-4 block hover:bg-[#f1f1f1] cursor-pointer'>Recebido</div>
+            <div onClick={()=> setIsOpenPago(true)} className='p-4 block hover:bg-[#f1f1f1] cursor-pointer'>Pago</div>
+          </div>
           </>
         )
+      }
+      {
+        isOpenRecebido === true ? (
+          <ModalCadastroRecebido isOpen={isOpenRecebido} setIsOpen={setIsOpenRecebido}  />
+        ) : null
+      }
+      {
+        isOpenPago === true ? (
+          <ModalCadastroPago isOpen={isOpenPago} setIsOpen={setIsOpenPago} />
+        ) : null
       }
     </div>
   )

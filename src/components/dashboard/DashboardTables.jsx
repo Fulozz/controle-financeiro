5c96c3d387cf2b245dc3bb9d42cf980704ccb51c
Thiago Silva Andrade <92411755+Fulozz@users.noreply.github.com>
2025-01-17 12:04:02 -0300

    Add AgendadoTable component and integrate it into DashboardTables for enhanced expense tracking

--- src/components/dashboard/DashboardTables.jsx
+++ src/components/dashboard/DashboardTables.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import GastosTable from './tables/GastosTable'
-
+import AgendadoTable from './tables/AgendadoTable';
 const DashboardTables = () => {
   return (
     <div className='flex flex-1 flex-col gap-4 p-4 pt-0'>
         <div className="grid auto-rows-min gap-4 md:grid-cols-2">
-            <GastosTable />
+            <AgendadoTable />
             <GastosTable />
         </div>
     </div>
---
+++ src/components/dashboard/tables/AgendadoTable.jsx
@@ -0,0 +1,133 @@
+import React, { useState } from "react";
+import {  Clock, Check, X } from "lucide-react";
+const AgendadoTable = () => {
+  const gastos = [
+    {
+      id: 1,
+      data: "2025-01-20",
+      status: "agendado",
+      descricao: "Compra de supermercado",
+      valor: "R$ 150,00",
+    },
+    {
+      id: 2,
+      data: "2025-01-25",
+      status: "recorrente",
+      descricao: "Gasolina",
+      valor: "R$ 200,00",
+    },
+    {
+      id: 3,
+      data: "2025-01-25",
+      status: "recorrente",
+      descricao: "Aluguel",
+      valor: "R$ 1.200,00",
+    },
+    {
+      id: 4,
+      data: "2025-01-25",
+      status: "recorrente",
+      descricao: "Conta de luz",
+      valor: "R$ 100,00",
+    },
+    {
+      id: 5,
+      data: "2025-01-25",
+      status: "recorrente",
+      descricao: "Conta de água",
+      valor: "R$ 80,00",
+    },
+    {
+      id: 6,
+      data: "2025-01-25",
+      status: "agendado",
+      descricao: "Internet",
+      valor: "R$ 120,00",
+    },
+    {
+      id: 7,
+      data: "2025-01-25",
+      status: "recorrente",
+      descricao: "Academia",
+      valor: "R$ 90,00",
+    },
+    {
+      id: 8,
+      data: "2025-01-25",
+      status: "recorrente",
+      descricao: "Restaurante",
+      valor: "R$ 250,00",
+    },
+    {
+      id: 9,
+      data: "2025-01-21",
+      status: "agendado",
+      descricao: "Cinema",
+      valor: "R$ 50,00",
+    },
+    {
+      id: 10,
+      data: "2025-01-22",
+      status: "agendado",
+      descricao: "Farmácia",
+      valor: "R$ 30,00",
+    },
+  ];
+  const limit = 10;
+
+  return (
+    <div className="overflow-w-auto border-white border-2 rounded-lg p-2">
+      <table className="min-w-full divide-y divide-gray-700 dark:divide-gray-200">
+        <caption className="">Gastos Recorrentes/Agendados</caption>
+        <thead>
+          <tr>
+            <th className="px-6 py-3 text-left text-xs font-medium text-black dark:text-white uppercase tracking-wider">
+              Data
+            </th>
+            <th className="px-6 py-3 text-left text-xs font-medium text-black dark:text-white uppercase tracking-wider">
+              Status
+            </th>
+            <th className="px-6 py-3 text-left text-xs font-medium text-black dark:text-white uppercase tracking-wider">
+              Categoria
+            </th>
+            <th className="px-6 py-3 text-left text-xs font-medium text-black dark:text-white uppercase tracking-wider">
+              valor
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {gastos.slice(0, limit).map((gasto) => (
+            <tr key={gasto.id}>
+              <td>{gasto.data}</td>
+              {gasto.status === "recorrente" ? (
+                <td className="flex  items-center">
+                  <Check  className="h-5 w-5 mr-2 text-green-500 " />
+                  {gasto.status}
+                </td>
+              ) : 
+              gasto.status === "agendado" ? (
+                <td className="flex items-center">
+                  <Clock className="h-5 w-5 mr-2 text-yellow-400" />
+                  {gasto.status}
+                </td>
+              ) : gasto.status === "cancelado" ? (
+                <td className="flex items-center">
+                  <X className="h-5 w-5 mr-2 text-red-500" />
+                  {gasto.status}
+                </td>
+              ) : null
+              }
+              <td>{gasto.descricao}</td>
+              <td className="text-red-500">-{gasto.valor}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <div className="text-center pt-2 text-gray-500 dark:text-gray-400">
+        Exibindo os ultimos {limit} itens
+      </div>
+    </div>
+  );
+};
+
+export default AgendadoTable;

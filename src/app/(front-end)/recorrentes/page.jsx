1cfdcd2e6ac5a7f997c14a28751911ec5d922ffa
Thiago Silva Andrade <92411755+Fulozz@users.noreply.github.com>
2025-01-22 11:30:41 -0300

    Add TableActions component and integrate it into the recorrentes page

--- src/app/(front-end)/recorrentes/page.jsx
+++ src/app/(front-end)/recorrentes/page.jsx
@@ -3,11 +3,13 @@ import React from 'react'
 
 import DataTable from '@/common/DataTable';
 import { useSidebar } from '@/hooks/useSidebar';
+import TableActions from '@/common/TableActions';
 const page = () => {
   const {isActive} = useSidebar()
   return (
     <div className={`  justify-center ${isActive === false ? "px-0" : "pl-[270px] pt-2"}`}>
       <div className="px-5">
+        <TableActions title="Gastos recorrentes" />
         <DataTable />
       </div>    
     </div>

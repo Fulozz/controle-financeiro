aad245358c23723a0abdd2b8fb4d1c811aac7577
Thiago Silva Andrade <92411755+Fulozz@users.noreply.github.com>
2025-01-22 11:38:43 -0300

    Add PageHeader component and integrate it into the recorrentes page; enhance layout with new heading

--- src/app/(front-end)/recorrentes/page.jsx
+++ src/app/(front-end)/recorrentes/page.jsx
@@ -4,12 +4,19 @@ import React from 'react'
 import DataTable from '@/common/DataTable';
 import { useSidebar } from '@/hooks/useSidebar';
 import TableActions from '@/common/TableActions';
+import PageHeader from '@/common/PageHeader';
 const page = () => {
   const {isActive} = useSidebar()
+  const href = "/dashboard/categories/new";
+  const linkTitle = "Nova Categoria";
   return (
     <div className={`  justify-center ${isActive === false ? "px-0" : "pl-[270px] pt-2"}`}>
       <div className="px-5">
-        <TableActions title="Gastos recorrentes" />
+        <div className="py-4">
+          <PageHeader href={href} linkTitle={linkTitle} />
+          <TableActions title="Gastos recorrentes" />
+          
+        </div>
         <DataTable />
       </div>    
     </div>

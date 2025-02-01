ce63282f68cab820dc9596f01866ac977aa6967a
Thiago Silva Andrade <92411755+Fulozz@users.noreply.github.com>
2025-01-31 22:01:06 -0300

    Refactor InstallBanner to conditionally display based on installation status

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -1,22 +1,33 @@
 "use client"
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function InstallBanner() {
-  const [isShown, setIsShown] = useState(true); // Você pode ajustar essa lógica para mostrar o banner em condições específicas
+    const [isShown, setIsShown] = useState(false);
 
-  const handleInstall = () => {
-    // Lógica para mostrar as instruções de instalação
-    alert('Toque e segure no ícone do seu navegador e escolha "Adicionar à tela inicial"');
-  };
+    useEffect(() => {
+        const isBannerInstalled = localStorage.getItem('isBannerInstalled') === 'true';
+        setIsShown(!isBannerInstalled);
+    }, []);
 
-  return (
-    <div className="fixed flex flex-col bottom-0 left-0 right-0 pb-[170px] h-10  bg-blue-500 text-white p-4 text-center">
-      Adicione este site à sua tela inicial para um acesso mais rápido!
-      <button onClick={handleInstall} className="bg-white text-blue-500 rounded-md px-4 py-2">
-        Instalar
-      </button>
-    </div>
-  );
+    const handleInstall = () => {
+        // Lógica para mostrar as instruções de instalação
+        alert('Toque e segure no ícone do seu navegador e escolha "Adicionar à tela inicial"');
+        localStorage.setItem('isBannerInstalled', 'true');
+        setIsShown(false);
+    };
+
+    if (!isShown) {
+        return null;
+    }
+
+    return (
+        <div className="fixed flex flex-col bottom-0 left-0 right-0 pb-[170px] h-10 bg-blue-500 text-white p-4 text-center">
+            Adicione este site à sua tela inicial para um acesso mais rápido!
+            <button onClick={handleInstall} className="bg-white text-blue-500 rounded-md px-4 py-2">
+                Instalar
+            </button>
+        </div>
+    );
 }
 
 export default InstallBanner;

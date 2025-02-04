acc563760528c2f32b5a865039da55b8b192d57e
Thiago Silva Andrade <92411755+Fulozz@users.noreply.github.com>
2025-02-03 22:13:19 -0300

    Refactor DashboardCard component; adjust grid layout to improve responsiveness with updated column configuration

--- src/components/dashboard/DashboardCard.jsx
+++ src/components/dashboard/DashboardCard.jsx
@@ -53,7 +53,7 @@ const DashboardCard = ({ mesRef, data, isLoading, error}) => {
   return (
     <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
       <h1 className='text-[1.8rem] font-bold text-slate-700 dark:text-slate-300'>Visão Geral das Contas</h1>
-      <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+      <div className="grid auto-rows-min gap-4 md:grid-cols-2 lg:grid-cols-3">
         <div className="h-[210px] w-full rounded-xl bg-white shadow-lg dark:bg-[#18181A] flex flex-col items-center justify-center">
           <p className='text-base md:text-[1.4rem] '>Total recebido - <span className='font-bold'>{mes}</span> </p>
           <h2 className='text-[2.2rem] text-blue-500 font-bold'>{totalRecebido ? (formatValue(totalRecebido)): ('R$00,00')}</h2>

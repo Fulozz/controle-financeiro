2a2b8bac70e1f732d2ba96988962ba62af10b0fb
Thiago Silva Andrade <92411755+Fulozz@users.noreply.github.com>
2025-02-03 22:09:56 -0300

    Refactor DashboardCard component; replace aspect ratio with fixed height for consistency in card layout

--- src/components/dashboard/DashboardCard.jsx
+++ src/components/dashboard/DashboardCard.jsx
@@ -54,15 +54,15 @@ const DashboardCard = ({ mesRef, data, isLoading, error}) => {
     <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
       <h1 className='text-[1.8rem] font-bold text-slate-700 dark:text-slate-300'>Visão Geral das Contas</h1>
       <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-        <div className="aspect-video rounded-xl bg-white shadow-lg dark:bg-[#18181A] flex flex-col items-center justify-center">
+        <div className="h-[210px] w-full rounded-xl bg-white shadow-lg dark:bg-[#18181A] flex flex-col items-center justify-center">
           <p className='text-base md:text-[1.4rem] '>Total recebido - <span className='font-bold'>{mes}</span> </p>
           <h2 className='text-[2.2rem] text-blue-500 font-bold'>{totalRecebido ? (formatValue(totalRecebido)): ('R$00,00')}</h2>
         </div>
-        <div className="aspect-video rounded-xl bg-white shadow-lg dark:bg-[#18181A] flex flex-col items-center justify-center">
+        <div className="h-[210px] w-full rounded-xl bg-white shadow-lg dark:bg-[#18181A] flex flex-col items-center justify-center">
           <p className='text-base md:text-[1.4rem] '>Total pago - <span className='font-bold'>{mes}</span> </p>
           <h2 className='text-[2.2rem] text-red-500 font-bold'>-{totalPago ? (formatValue(totalPago)): ('R$00,00')}</h2>
         </div>
-        <div className="aspect-video rounded-xl bg-white shadow-lg dark:bg-[#18181A] flex flex-col items-center justify-center">
+        <div className="h-[210px] w-full rounded-xl bg-white shadow-lg dark:bg-[#18181A] flex flex-col items-center justify-center">
           <p className='text-base md:text-[1.4rem] '>Saldo</p>
           <h2 className='text-[2.2rem] text-green-500 font-bold'>{saldo ? (formatValue(saldo)): ('R$00,00')}</h2>
         </div>
